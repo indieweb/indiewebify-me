b094f109963316f26354165a9ce48c1368971c0b
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-09-29 20:46:03 +0000

    Updated dependencies, added built-in server static asset routing

--- web/index.php
+++ web/index.php
@@ -1,10 +1,15 @@
 <?php
 
+namespace Indieweb\IndiewebifyMe;
+
 ob_start();
 require __DIR__ . '/../vendor/autoload.php';
 ob_end_clean();
 
+use Guzzle;
+use mf2;
 use Silex;
+use Symfony\Component\HttpFoundation as Http;
 
 function renderTemplate($template, array $data = []) {
 	$templatePath = __DIR__ . '/../templates/' . $template . '.php';
@@ -30,10 +35,23 @@ function render($template, array $data = []) {
 	return $out;
 }
 
+// Web server setup
+
+// Route static assets from CLI server
+if (PHP_SAPI === 'cli-server') {
+	if (file_exists(__DIR__ . $_SERVER['REQUEST_URI']) and !is_dir(__DIR__ . $_SERVER['REQUEST_URI'])) {
+		return false;
+	}
+}
+
 $app = new Silex\Application();
 
 $app->get('/', function () {
 	return render('index.html');
+});
+
+$app->get('/validate-rels', function (Http\Request $request) {
+	
 });
 
 $app->run();
