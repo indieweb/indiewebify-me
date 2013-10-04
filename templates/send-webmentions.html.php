2f1a5ffa47fd987b30647208f9599658901f3d5c
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-10-04 15:10:19 +0000

    Updated link to IWC webmention page

--- templates/send-webmentions.html.php
+++ templates/send-webmentions.html.php
@@ -8,7 +8,7 @@
 	<ul>
 		<li>Send webmentions and pingbacks manually using the form below</li>
 		<li>Send webmentions from the command line <a href="http://indiewebcamp.com/webmention#How_to_Test_Webmentions">using Curl</a> or <a href="https://github.com/vrypan/webmention-tools">webmention-tools</a></li>
-		<li>Make your publishing software send webmentions for you automatically using one of the <a href="http://indiewebcamp.com/webmention#Webmention-related_libraries_and_tools">open source libraries</a></li>
+		<li>Make your publishing software send webmentions for you automatically using one of the <a href="http://indiewebcamp.com/webmention#Implementations">open source libraries</a></li>
 		<li>Or, get down and dirty with the <a href="http://webmention.org">WebMention spec</a> and implement it yourself</li>
 	</ul>
 	
