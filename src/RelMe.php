2e044c91d9e0a836c98367da5f37e07129caf27f
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-10-07 21:22:51 +0000

    Added tests, draft implementation of URL normalisation and unparsing

--- src/RelMe.php
+++ src/RelMe.php
@@ -2,6 +2,31 @@
 
 namespace IndieWeb;
 
-function normaliseUrl($url) {
+/**
+ * Unparse URL
+ * 
+ * Given an assoc. array of the form produced by parse_url, return a string
+ * 
+ * Adapted from http://www.php.net/manual/en/function.parse-url.php#106731
+ * 
+ * @param  array $parsed_url
+ * @return string
+ */
+function unparseUrl(array $parsed_url) {
+	$user = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+	$pass = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
 	
+	return implode('', array(
+		isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '',
+		isset($parsed_url['host']) ? $parsed_url['host'] : '',
+		isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '',
+		($user || $pass) ? "$pass@" : '',
+		isset($parsed_url['path']) ? $parsed_url['path'] : '/',
+		isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '',
+		isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '',
+ ));
+}
+
+function normaliseUrl($url) {
+	return unparseUrl(parse_url($url));
 }
--- tests/RelMeTest.php
+++ tests/RelMeTest.php
@@ -11,6 +11,10 @@ use IndieWeb as IW;
  * @author barnabywalters
  */
 class RelMeTest extends PHPUnit_Framework_TestCase {
+	public function testUnparseUrl() {
+		$this->assertEquals('http://example.com/', IW\unparseUrl(parse_url('http://example.com')));
+	}
+	
 	public function testNormaliseUrl() {
 		$this->assertEquals('http://example.com/', IW\normaliseUrl('http://example.com'));
 		$this->assertEquals('http://example.com/?thing=1', IW\normaliseUrl('http://example.com?thing=1'));
