177e37cb2b2d78563c2db40794cbecb35b9d4bca
Barnaby Walters <barnaby@waterpigs.co.uk>
2014-01-04 18:21:05 +0000

    Improved capitalisation

--- templates/silo-hint.html.php
+++ templates/silo-hint.html.php
@@ -7,27 +7,27 @@
 			<li><a href="http://en.support.wordpress.com/domains/">Wordpress Support article on Domains</a></li>
 			<li><a href="https://store.wordpress.com/premium-upgrades/custom-domains/">Wordpress.com custom domain premium upgrade</a></li>
 		</ol>
-		<p>Once you’ve got your own domain, check out the IndieWebCamp <a href="http://indiewebcamp.com/Wordpress">WordPress Wiki Page</a> for tips and plugins for adding things like webmentions and microformats to your site.</p>
+		<p>Once you’ve got your own domain, check out the IndieWebCamp <a href="http://indiewebcamp.com/Wordpress">WordPress wiki page</a> for tips and plugins for adding things like webmentions and microformats to your site.</p>
 	</div>
 
 <?php elseif (isTumblrDomain($url)): ?>
 	<div class="silo-hint">
 		<p>It looks like your site is hosted on <a href="http://indiewebcamp.com/Tumblr">Tumblr.com Silo</a> without a custom domain name. In order to really own your content you need to own your URLs, but don’t worry — Tumblr has <a href="http://www.tumblr.com/docs/en/custom_domains">an article</a> demonstrating how to give your web presence your own domain.</p>
-		<p>Once you’ve got your own domain, check out the IndieWebCamp <a href="http://indiewebcamp.com/Tumblr">Tumblr Wiki Page</a> for tips on adding indieweb functionality to your site, or exporting your data elsewhere.</p>
+		<p>Once you’ve got your own domain, check out the IndieWebCamp <a href="http://indiewebcamp.com/Tumblr">Tumblr wiki page</a> for tips on adding indieweb functionality to your site, or exporting your data elsewhere.</p>
 	</div>
 
 <?php elseif (isGithubDomain($url)): ?>
 	<div class="silo-hint">
 		<p>It looks like your site is hosted on <a href="http://indiewebcamp.com/Github">Github.io Silo</a> without a custom domain name. In order to really own your content you need to own your URLs, but don’t worry — Github has <a href="https://help.github.com/articles/setting-up-a-custom-domain-with-pages">an article</a> demonstrating how to give your web presence your own domain.</p>
-		<p>Once you’ve got your own domain, check out the IndieWebCamp <a href="http://indiewebcamp.com/Github">Github Wiki Page</a> for tips on adding indieweb functionality to your site.</p>
+		<p>Once you’ve got your own domain, check out the IndieWebCamp <a href="http://indiewebcamp.com/Github">Github wiki page</a> for tips on adding indieweb functionality to your site.</p>
 	</div>
 
 <?php elseif (isset($bloggingSoftware) and $bloggingSoftware === 'wordpress'): ?>
-	<p>It looks like you’re using WordPress to power your site — check out the IndieWebCamp <a href="http://indiewebcamp.com/Wordpress">WordPress Wiki Page</a> for tips on how to indiewebify your WordPress site!</p>
+	<p>It looks like you’re using WordPress to power your site — check out the IndieWebCamp <a href="http://indiewebcamp.com/Wordpress">WordPress wiki page</a> for tips on how to indiewebify your WordPress site!</p>
 
 <?php elseif (isset($bloggingSoftware) and $bloggingSoftware === 'mediawiki'): ?>
-	<p>It looks like you’re using MediaWiki to power your site — check out the IndieWebCamp <a href="http://indiewebcamp.com/MediaWiki">MediaWiki Page</a> for tips on how to indiewebify your MediaWiki site!</p>
+	<p>It looks like you’re using MediaWiki to power your site — check out the IndieWebCamp <a href="http://indiewebcamp.com/MediaWiki">MediaWiki wiki page</a> for tips on how to indiewebify your MediaWiki site!</p>
 
 <?php elseif (isset($bloggingSoftware) and $bloggingSoftware === 'idno'): ?>
-	<p>It looks like you’re using idno to power your site — check out the IndieWebCamp <a href="http://indiewebcamp.com/Idno">idno Wiki Page</a> for tips on how to indiewebify your idno site!</p>
+	<p>It looks like you’re using idno to power your site — check out the IndieWebCamp <a href="http://indiewebcamp.com/Idno">idno wiki page</a> for tips on how to indiewebify your idno site!</p>
 <?php endif ?>
