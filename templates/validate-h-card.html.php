d25636056f5b79e4f56575cff71fab95ed63a178
Martijn van der Ven <martijn@vanderven.se>
2020-05-02 23:52:56 +0200

    If inside if causing issues, should be elseif

--- templates/validate-h-card.html.php
+++ templates/validate-h-card.html.php
@@ -93,7 +93,7 @@
 			<div class="preview-h-card preview-block">
 				<?php if (Mf2\hasProp($hCard, 'photo')): ?>
 				<img class="photo-block" src="<?= Mf2\getProp($hCard, 'photo')?>" alt="" />
-				<?php if (Mf2\hasProp($hCard, 'logo')): ?>
+				<?php elseif (Mf2\hasProp($hCard, 'logo')): ?>
 				<img class="logo-block" src="<?= Mf2\getProp($hCard, 'logo')?>" alt="" />
 				<?php else: ?>
 				<div class="empty-property-block photo-block">
